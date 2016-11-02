1e639ca1cd0282a86dfdad87c9be5250375bded0
Marcius <marcio.hsdl@gmail.com>
2016-11-02 20:40:11 -0300

    Update-02/11/2016--9

--- app/src/main/java/mpoo/bsi/ufrpe/organictrade/OrganicTradeControler/GUI/RegisterTentItemActivity.java
+++ app/src/main/java/mpoo/bsi/ufrpe/organictrade/OrganicTradeControler/GUI/RegisterTentItemActivity.java
@@ -5,12 +5,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import mpoo.bsi.ufrpe.organictrade.Infra.Persistencia.DatabaseHelper;
 import mpoo.bsi.ufrpe.organictrade.Infra.Session;
+import mpoo.bsi.ufrpe.organictrade.OrganicTradeControler.Dominio.Product;
 import mpoo.bsi.ufrpe.organictrade.OrganicTradeControler.Dominio.TentItems;
+import mpoo.bsi.ufrpe.organictrade.OrganicTradeControler.Persistencia.ProductPersistence;
 import mpoo.bsi.ufrpe.organictrade.OrganicTradeControler.Persistencia.TentItemsPersistence;
 import mpoo.bsi.ufrpe.organictrade.R;
 
@@ -18,25 +22,30 @@ public class RegisterTentItemActivity extends AppCompatActivity {
 
     private SQLiteDatabase db;
     private DatabaseHelper banco = Session.getDbAtual();
+    private ProductPersistence product = new ProductPersistence();
+    private Spinner nameProduct;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_tent_item);
         Session.setContext(getBaseContext());
+        String[] lista = product.getNameProducts();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,product.getNameProducts());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        nameProduct = (Spinner) findViewById(R.id.registerTentItemSpiProductName);
+        nameProduct.setAdapter(adapter);
     }
 
     public void cadastrarProduto(View v) {
         TentItemsPersistence crud = new TentItemsPersistence();
 
-
-        //Spinner nameProduct = (Spinner) findViewById(R.id.registerTentItemSpiProductName);
         EditText unidade = (EditText) findViewById(R.id.registerTentItemEdtProductUnity);
         EditText quantidade = (EditText) findViewById((R.id.registerTentItemEdtProductAmount));
         EditText price = (EditText) findViewById(R.id.registerTentItemEdtProductPrice);
 
-        String nameProductString = "Banana";
-        //nameProduct.getSelectedItem().toString();
+        String nameProductString = nameProduct.getSelectedItem().toString();
         String unidadeString = unidade.getText().toString();
         String quantidadeString = quantidade.getText().toString();
         String priceString = price.getText().toString();
