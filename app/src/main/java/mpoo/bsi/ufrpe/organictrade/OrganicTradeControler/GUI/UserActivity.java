72ada9bc9c84ed32f93b5687a5ea502f643677b0
Marcius <marcio.hsdl@gmail.com>
2016-11-16 05:15:01 -0300

    Update-16/11/2016 -- 01

--- app/src/main/java/mpoo/bsi/ufrpe/organictrade/OrganicTradeControler/GUI/UserActivity.java
+++ app/src/main/java/mpoo/bsi/ufrpe/organictrade/OrganicTradeControler/GUI/UserActivity.java
@@ -1,7 +1,11 @@
 package mpoo.bsi.ufrpe.organictrade.OrganicTradeControler.GUI;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -24,6 +28,7 @@ import mpoo.bsi.ufrpe.organictrade.R;
 
 public class UserActivity extends AppCompatActivity {
     private long lastBackPressTime = 0;
+    private static int RESULT_LOAD_IMAGE = 1;
     private Toast toast;
 
     @Override
@@ -37,6 +42,22 @@ public class UserActivity extends AppCompatActivity {
                 toast.cancel();
             }
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            ImageView imageView = (ImageView) findViewById(R.id.profilePicture);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
     }
 
@@ -136,6 +157,7 @@ public class UserActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -148,10 +170,18 @@ public class UserActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        final ImageView imageView = (ImageView) findViewById(R.id.profilePicture);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
     }
 
-    private void displayToastAboveButton(View v, int messageId)
-    {
+    private void displayToastAboveButton(View v, int messageId) {
         int xOffset = 0;
         int yOffset = 0;
         Rect gvr = new Rect();
